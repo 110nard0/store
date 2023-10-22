abba40ac6e606140bf12840fbd16e93df4bfc9db
psych0wh1z <ikeagudike@gmail.com>
2023-10-22 08:35:48 +0100

    Fix deployment issues

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,7 @@
-# from allauth.account.views import ConfirmEmailView
 from django.urls import include, path, re_path
-# from dj_rest_auth.registration.views import RegisterView as DefaultRegisterView, VerifyEmailView
-# from dj_rest_auth.views import LoginView, LogoutView
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# from .serializers import CustomRegisterSerializer
 from .views import (CustomUserRegister, CustomUserList,
                     LoginView, ProductViewSet, WaitlistUserViewSet)
 from . import views
