324b2d4dfcca331b18ca5c6b0716106c8775c74d
psych0wh1z <ikeagudike@gmail.com>
2023-10-25 22:21:15 +0100

    Switch production database to SQLite DB

--- store/settings.py
+++ store/settings.py
@@ -25,10 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 DEBUG = False
 
 # ALLOWED_HOSTS = ['trgst.com', 'www.trgst.com']
 ALLOWED_HOSTS = [
+    # '127.0.0.1',
     'trgst-product-app.onrender.com',
 ]
 
@@ -92,16 +94,17 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.parse(config("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/var/data/db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse(config("DATABASE_URL"))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
