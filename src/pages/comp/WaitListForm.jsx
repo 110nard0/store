5f32af4eaa114196a22ebc3a3b850440fc6ce124
psych0wh1z <ikeagudike@gmail.com>
2023-11-06 18:33:01 +0100

    Add public icons

--- src/pages/comp/WaitListForm.jsx
+++ src/pages/comp/WaitListForm.jsx
@@ -1,27 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { waitListSchema } from "../../schemas";
-
-import { BiErrorCircle } from "react-icons/bi";
+import { waitListSchema } from "@schemas";
 import { AiFillHeart } from "react-icons/ai";
+import { newWaitUser } from "../../services";
+import InputGroup from "@components/InputGroup";
 
 const WaitListForm = () => {
-  // -------------------------STATE FOR THE SUCCESS CONTENT-----------------------------------------
+  // HDR: INIIALISERS -------------------------STATE FOR THE SUCCESS CONTENT-----------------------------------------
   const [showSuccess, setShowSuccess] = useState(false);
+  const [formError, setFormError] = useState("");
 
-  // -------------------------SUBMISSION OF THE FORM DATA-----------------------------------------
-  const onSubmit = (formData) => {
-    console.log(formData);
+  // HDR: ACTIONS -------------------------SUBMISSION OF THE FORM DATA-----------------------------------------
+  const onSubmit = async (formData) => {
+    try {
+      await newWaitUser(formData);
+      setShowSuccess(true);
+      reset();
+    } catch (err) {
+      if (err.response && err.response.status === 400) {
+        setFormError(err.response.data.error);
+      }
+    }
   };
 
-  // const isSubmitSuccessful = true;
+  //SUB: ------CLEAR ERROR MESSAGE--------------
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFormError("");
+    }, 8000);
 
-  // -------------------------FORM VALIDATION-----------------------------------------
+    return () => clearTimeout(timer);
+  }, [formError]);
+
+  //SUB: ------FORM VALIDATION--------------
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitSuccessful },
+    formState: { errors },
     reset,
   } = useForm({
     resolver: zodResolver(waitListSchema),
@@ -31,16 +47,7 @@ const WaitListForm = () => {
     },
   });
 
-  // -------------------------DISPLAY THE SUCCESS CONTENT AFTER SUBMITTING-----------------------------------------
-
-  useEffect(() => {
-    if (isSubmitSuccessful) {
-      reset();
-      setShowSuccess(true);
-    }
-  }, [isSubmitSuccessful]);
-
-  // ================================================================================================
+  //HDR: JSX ================================================================================================
   return (
     <form
       name="access-form"
@@ -50,44 +57,33 @@ const WaitListForm = () => {
       {/* ================================================================================================ */}
       {!showSuccess && (
         <div className="register_form">
-          <h6>
-            To gain access to our clothing on launch day, fill the form to
-            receive your password.
-          </h6>
-          <div className={`input-group ${errors.name && "error"}`}>
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Enter your name"
-              {...register("name")}
-              autoComplete="on"
-            />
-
-            {errors.name && (
-              <span>
-                <BiErrorCircle /> {errors.name?.message}
-              </span>
-            )}
-          </div>
-          <div className={`input-group ${errors.email && "error"}`}>
-            <label htmlFor="email">Email Address</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              autoComplete="on"
-              placeholder="Enter your email address"
-              {...register("email")}
-            />
-            {errors.email && (
-              <span>
-                <BiErrorCircle /> {errors.email?.message}
-              </span>
-            )}
-          </div>
-
+          <p>
+            To gain access to our clothing on launch day, fill the form and get
+            up to 10% discount on your first purchase
+          </p>
+          <InputGroup
+            type="text"
+            id="name"
+            name="name"
+            autoComplete="on"
+            placeholder="Enter your name"
+            title="Name"
+            error={errors.name}
+            errormessage={errors.name?.message}
+            {...register("name")}
+          />
+          <InputGroup
+            type="email"
+            id="email"
+            name="email"
+            autoComplete="on"
+            placeholder="Enter your email address"
+            title="Email Address"
+            error={errors.email || formError}
+            errormessage={errors.email?.message}
+            {...register("email")}
+          />
+          {formError && <em className="form_error">{formError}</em>}
           <button className="waitlist_form-btn" type="submit">
             Get Password
           </button>
@@ -103,10 +99,10 @@ const WaitListForm = () => {
           </div>
 
           <h3 className="success_title">You are on the list</h3>
-          <h6 className="success_subtitle">
+          <p className="success_subtitle">
             Thank you for joining us. You will receive your password on launch
             day.
-          </h6>
+          </p>
         </div>
       )}
     </form>
