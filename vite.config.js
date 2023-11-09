f784bc508592fb4da5d6774af78af72f021845ea
psych0wh1z <ikeagudike@gmail.com>
2023-11-09 07:47:09 +0100

    Modify vercel environment

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,29 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import autoprefixer from "autoprefixer";
 
+import path from "node:path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
       plugins: [autoprefixer],
+    },
+  },
+  resolve: {
+    alias: {
+      "@asset": path.resolve(__dirname, "./src/assets/styles"),
+      "@images": path.resolve(__dirname, "./src/assets/images"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@data": path.resolve(__dirname, "./src/data"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@routing": path.resolve(__dirname, "./src/Routing"),
+      "@schemas": path.resolve(__dirname, "./src/schemas"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
     },
   },
 });
